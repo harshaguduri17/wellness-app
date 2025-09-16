7dc9c36a55ecc8b452d20631eff5489ca9370834
harshaguduri17 <harshaguduri24@gmail.com>
2025-09-16 20:45:03 +0530

    build: initial project structure

---
+++ backend/mywellness/src/main/java/com/wellness/mywellness/MywellnessApplication.java
@@ -0,0 +1,13 @@
+package com.wellness.mywellness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywellnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywellnessApplication.class, args);
+	}
+
+}
---
+++ backend/mywellness/src/test/java/com/wellness/mywellness/MywellnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellness.mywellness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywellnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
